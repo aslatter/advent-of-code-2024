3045b51fe4c7ca56966bc494afcc0e427195c07e
Antoine Latter <aslatter@gmail.com>
2024-12-15 20:43:44 -0600

    day 6 part 2

--- day6/main.go
+++ day6/main.go
@@ -4,6 +4,8 @@ import (
 	"aoc/util"
 	"fmt"
 	"io"
+	"maps"
+	"slices"
 )
 
 func main() {
@@ -11,6 +13,7 @@ func main() {
 }
 
 func mainErr(r io.Reader) error {
+	// parse the input
 	var level [][]bool
 	var startX, startY int
 
@@ -36,10 +39,16 @@ func mainErr(r io.Reader) error {
 	}
 	s.setPosition(startX, startY, orientationNorth)
 
-	for s.next() {
-	}
+	// find positions to evaluate
+	trials := findBasePositions(&s)
 
-	result := len(s.visited)
+	// evaluate if adding a barrier would trap the guard
+	var result int
+	for _, p := range trials {
+		if evaluateIntervention(&s, p) {
+			result++
+		}
+	}
 
 	fmt.Printf("result: %d\n", result)
 
@@ -51,23 +60,60 @@ type pos struct {
 	y int
 }
 
-type state struct {
-	level     [][]bool
-	visited   map[pos]bool
+type posAndDirection struct {
 	x         int
 	y         int
 	direction orientation
 }
 
-func (s *state) setPosition(x int, y int, direction orientation) {
-	if s.visited == nil {
-		s.visited = map[pos]bool{}
+type state struct {
+	level     [][]bool
+	visited   map[posAndDirection]bool
+	x         int
+	y         int
+	direction orientation
+	result    disposition
+}
+
+func (s *state) clone() *state {
+	var newS state
+
+	newS.level = make([][]bool, 0, len(s.level))
+	for _, r := range s.level {
+		newS.level = append(newS.level, slices.Clone(r))
 	}
 
-	s.visited[pos{x: x, y: y}] = true
+	newS.visited = maps.Clone(s.visited)
+
+	newS.x = s.x
+	newS.y = s.y
+	newS.direction = s.direction
+
+	return &newS
+}
+
+func (s *state) setPosition(x int, y int, direction orientation) bool {
+	if s.visited == nil {
+		s.visited = map[posAndDirection]bool{}
+	}
+
+	l := posAndDirection{
+		x:         x,
+		y:         y,
+		direction: direction,
+	}
+
+	if s.visited[l] {
+		return false
+	}
+
+	s.visited[l] = true
+
 	s.x = x
 	s.y = y
 	s.direction = direction
+
+	return true
 }
 
 func (s *state) next() bool {
@@ -88,18 +134,24 @@ func (s *state) next() bool {
 
 	// did we move off the map?
 	if nextX < 0 || nextY < 0 || nextX == len(s.level[0]) || nextY == len(s.level) {
+		s.result = dispositionExited
 		return false
 	}
 
 	// did we attempt to move into an obstruction?
 	if s.level[nextY][nextX] {
+		// reset position, turn to the right
 		nextX = s.x
 		nextY = s.y
 		nextDirection++
 		nextDirection %= 4
 	}
 
-	s.setPosition(nextX, nextY, nextDirection)
+	// did we hit a loop?
+	if !s.setPosition(nextX, nextY, nextDirection) {
+		s.result = dispositionLooped
+		return false
+	}
 
 	// keep going
 	return true
@@ -109,7 +161,58 @@ type orientation int
 
 const (
 	orientationNorth orientation = iota
-	orientationEast  orientation = iota
-	orientationSouth orientation = iota
-	orientationWest  orientation = iota
+	orientationEast
+	orientationSouth
+	orientationWest
 )
+
+type disposition int
+
+const (
+	dispositionInProgress disposition = iota
+	dispositionExited
+	dispositionLooped
+)
+
+// findBasePositions finds positions visited by the guard if
+// we don't intervene. We don't need to evaluate any other positions
+// because those barriers will not be reachable by the guard.
+func findBasePositions(base *state) []pos {
+	s := base.clone()
+	for s.next() {
+	}
+	var result []pos
+	for pp := range s.visited {
+		result = append(result, pos{x: pp.x, y: pp.y})
+	}
+
+	// remove duplicates
+	slices.SortFunc(result, func(a, b pos) int {
+		d := a.x - b.x
+		if d == 0 {
+			return a.y - b.y
+		}
+		return d
+	})
+	result = slices.Compact(result)
+
+	return result
+}
+
+// evaluateIntervention returns 'true' if placing a barrier
+// at position 'pos' would result in trapping the guard in
+// a loop.
+func evaluateIntervention(base *state, p pos) bool {
+	s := base.clone()
+
+	// add barrier
+	s.level[p.y][p.x] = true
+
+	// run simulation
+	for s.next() {
+	}
+
+	// we either stopped because the guard left or
+	// because the guard is trapped.
+	return s.result == dispositionLooped
+}
