6dea2b3dae805b52e8a412b31ca4b1754eafb29a
Antoine Latter <aslatter@gmail.com>
2024-12-22 22:39:34 -0600

    day 8 part 2

--- day8/main.go
+++ day8/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"iter"
+	"math/big"
 )
 
 func main() {
@@ -12,6 +13,9 @@ func main() {
 }
 
 func mainErr(r io.Reader) error {
+
+	// parse the locations of all antennae, grouped
+	// by label.
 	antennae := map[byte][]pos{}
 
 	var y int
@@ -28,16 +32,12 @@ func mainErr(r io.Reader) error {
 	}
 	maxY := y - 1
 
+	// log all positions which are anti-nodes.
 	antinodes := map[pos]bool{}
 
 	for _, allPos := range antennae {
 		for pair := range pairs(allPos) {
-			a := antinode(pair[0], pair[1])
-			if a.x >= 0 && a.x <= maxX && a.y >= 0 && a.y <= maxY {
-				antinodes[a] = true
-			}
-			a = antinode(pair[1], pair[0])
-			if a.x >= 0 && a.x <= maxX && a.y >= 0 && a.y <= maxY {
+			for a := range nodes(maxX, maxY, pair[0], pair[1]) {
 				antinodes[a] = true
 			}
 		}
@@ -51,6 +51,8 @@ func mainErr(r io.Reader) error {
 
 type pos struct{ x, y int }
 
+// pairs returns all pair-wise matchings of the points
+// in 'x'.
 func pairs(x []pos) iter.Seq[[2]pos] {
 	return func(yield func([2]pos) bool) {
 		for i := range len(x) {
@@ -63,11 +65,52 @@ func pairs(x []pos) iter.Seq[[2]pos] {
 	}
 }
 
-func antinode(p1 pos, p2 pos) pos {
-	dx := p1.x - p2.x
-	dy := p1.y - p2.y
-	return pos{
-		x: p2.x - dx,
-		y: p2.y - dy,
+// nodes returns all points on a straight line between p1 and
+// p2, while also in-bounds on the map.
+func nodes(maxX int, maxY int, p1 pos, p2 pos) iter.Seq[pos] {
+	return func(yield func(pos) bool) {
+		dx := p2.x - p1.x
+		dy := p2.y - p1.y
+		// too lazy to do gcd
+		r := big.NewRat(int64(dx), int64(dy))
+		dx = int(r.Num().Int64())
+		dy = int(r.Denom().Int64())
+
+		// p1 is always in-line with p1 and p2
+		if !yield(p1) {
+			return
+		}
+
+		// p2 will be hit by moving from p1
+
+		n := 0
+		for {
+			n++
+			p := pos{
+				x: p1.x + n*dx,
+				y: p1.y + n*dy,
+			}
+			if p.x < 0 || p.y < 0 || p.x > maxX || p.y > maxY {
+				break
+			}
+			if !yield(p) {
+				return
+			}
+		}
+
+		n = 0
+		for {
+			n--
+			p := pos{
+				x: p1.x + n*dx,
+				y: p1.y + n*dy,
+			}
+			if p.x < 0 || p.y < 0 || p.x > maxX || p.y > maxY {
+				break
+			}
+			if !yield(p) {
+				return
+			}
+		}
 	}
 }
